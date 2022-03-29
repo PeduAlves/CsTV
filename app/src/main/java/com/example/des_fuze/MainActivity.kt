621426ea7547364db95b4457054e6c9ac26aa6c5
Pedro Alves <pedrosilveira@cc.ci.ufpb.br>
2022-03-29 10:21:00 -0300

    Criando o projeto e adicionando os cards de partidas

---
+++ app/src/main/java/com/example/des_fuze/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.des_fuze
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
